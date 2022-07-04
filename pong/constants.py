1e6e8e502fd40b0b1b1c23e0cd4df43d833e91bf
kelvin-AJ <aji22001@byui.edu>
2022-07-04 16:14:58 +0100

    INITIALIZE 😇

---
+++ pong/constants.py
@@ -0,0 +1,93 @@
+from game.casting.color import Color
+# -------------------------------------------------------------------------------------------------- 
+# GENERAL GAME CONSTANTS
+# -------------------------------------------------------------------------------------------------- 
+
+# GAME
+GAME_NAME = "Pong"
+FRAME_RATE = 60
+
+# SCREEN
+SCREEN_WIDTH = 1040
+SCREEN_HEIGHT = 680
+CENTER_X = SCREEN_WIDTH / 2
+CENTER_Y = SCREEN_HEIGHT / 2
+
+# FIELD
+FIELD_TOP = 60
+FIELD_BOTTOM = SCREEN_HEIGHT
+FIELD_LEFT = 0
+FIELD_RIGHT = SCREEN_WIDTH
+
+# FONT
+FONT_FILE = "batter/assets/fonts/zorque.otf"
+FONT_SIZE = 32
+
+# SOUND
+BOUNCE_SOUND = "batter/assets/sounds/boing.wav"
+WELCOME_SOUND = "batter/assets/sounds/start.wav"
+
+# TEXT
+ALIGN_CENTER = 0
+ALIGN_LEFT = 1
+ALIGN_RIGHT = 2
+
+# COLORS
+BLACK = Color(0, 0, 0)
+BLUE = Color(0, 93, 243)
+WHITE = Color(255, 255, 255)
+PURPLE = Color(255, 0, 255)
+
+# KEYS
+# UP AND DOWN FOR PLAYER 1 AND 2
+
+# SCENES
+NEW_GAME = 0
+IN_PLAY = 1
+GAME_OVER = 2
+
+# -------------------------------------------------------------------------------------------------- 
+# SCRIPTING CONSTANTS
+# -------------------------------------------------------------------------------------------------- 
+
+# PHASES
+INITIALIZE = 0
+LOAD = 1
+INPUT = 2
+UPDATE = 3
+OUTPUT = 4
+UNLOAD = 5
+RELEASE = 6
+
+# -------------------------------------------------------------------------------------------------- 
+# CASTING CONSTANTS
+# -------------------------------------------------------------------------------------------------- 
+
+# STATS
+
+# HUD
+HUD_MARGIN = 15
+PLAYER_1_SCORE = 0
+PLAYER_2_SCORE = 0
+PLAYER_1_SCORE_FORMAT = f"PLAYER 1: {PLAYER_1_SCORE}"
+PLAYER_2_SCORE_FORMAT = f"PLAYER 2: {PLAYER_2_SCORE}"
+
+# BALL
+BALL_GROUP = "balls"
+BALL_IMAGE = "batter/assets/images/000.png"
+BALL_WIDTH = 28
+BALL_HEIGHT = 28
+BALL_VELOCITY = 6
+
+# RACKET
+RACKET_GROUP = "rackets"
+RACKET_IMAGE = ""
+RACKET_WIDTH = 106
+RACKET_HEIGHT = 28
+RACKET_RATE = 6
+RACKET_VELOCITY = 7
+
+
+# DIALOG
+DIALOG_GROUP = "dialogs"
+ENTER_TO_START = "PRESS ENTER TO START"
---
+++ pong/game/directing/scene_manager.py
@@ -0,0 +1,158 @@
+# IMPORT STUFF HERE
+import csv
+from constants import *
+
+from game.casting.ball import Ball
+from game.casting.body import Body
+from game.casting.point import Point
+from game.casting.text import Text
+from game.casting.image import Image
+from game.casting.label import Label 
+from game.scripting.play_sound_action import PlaySoundAction
+from game.services.raylib.raylib_audio_service import RaylibAudioService
+from game.services.raylib.raylib_keyboard_service import RaylibKeyboardService
+from game.services.raylib.raylib_physics_service import RaylibPhysicsService
+from game.services.raylib.raylib_video_service import RaylibVideoService
+
+class SceneManager:
+    """The person in charge of setting up the cast and script for each scene."""
+    
+    AUDIO_SERVICE = RaylibAudioService()
+    KEYBOARD_SERVICE = RaylibKeyboardService()
+    PHYSICS_SERVICE = RaylibPhysicsService()
+    VIDEO_SERVICE = RaylibVideoService(GAME_NAME, SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    
+
+    def __init__(self):
+        pass
+
+    def prepare_scene(self, scene, cast, script):
+        if scene == NEW_GAME:
+            self._prepare_new_game(cast, script)
+        elif scene == IN_PLAY:
+            self._prepare_in_play(cast, script)
+        elif scene == GAME_OVER:    
+            self._prepare_game_over(cast, script)
+    
+    # ----------------------------------------------------------------------------------------------
+    # scene methods
+    # ----------------------------------------------------------------------------------------------
+    
+    def _prepare_new_game(self, cast, script):
+        self._add_stats(cast)
+        self._add_level(cast)
+        self._add_lives(cast)
+        self._add_score(cast)
+        self._add_ball(cast)
+        self._add_bricks(cast)
+        self._add_racket(cast)
+        self._add_dialog(cast, ENTER_TO_START)
+
+        self._add_initialize_script(script)
+        self._add_load_script(script)
+        script.clear_actions(INPUT)
+        self._add_output_script(script)
+        self._add_unload_script(script)
+        self._add_release_script(script)
+        
+
+    def _prepare_in_play(self, cast, script):
+        self._activate_ball(cast)
+        cast.clear_actors(DIALOG_GROUP)
+
+        script.clear_actions(INPUT)
+        script.add_action(INPUT, self.CONTROL_RACKET_ACTION)
+        self._add_update_script(script)
+        self._add_output_script(script)
+
+    def _prepare_game_over(self, cast, script):
+        self._add_ball(cast)
+        script.clear_actions(INPUT)
+        script.clear_actions(UPDATE)
+        self._add_output_script(script)
+
+    # ----------------------------------------------------------------------------------------------
+    # casting methods
+    # ----------------------------------------------------------------------------------------------
+    
+    def _activate_ball(self, cast):
+        ball = cast.get_first_actor(BALL_GROUP)
+        ball.release()
+
+    def _add_ball(self, cast):
+        cast.clear_actors(BALL_GROUP)
+        x = CENTER_X - BALL_WIDTH / 2
+        y = SCREEN_HEIGHT - RACKET_HEIGHT - BALL_HEIGHT  
+        position = Point(x, y)
+        size = Point(BALL_WIDTH, BALL_HEIGHT)
+        velocity = Point(0, 0)
+        body = Body(position, size, velocity)
+        image = Image(BALL_IMAGE)
+        ball = Ball(body, image, True)
+        cast.add_actor(BALL_GROUP, ball)
+
+
+    def _add_dialog(self, cast, message):
+        cast.clear_actors(DIALOG_GROUP)
+        text = Text(message, FONT_FILE, FONT_SIZE, ALIGN_CENTER)
+        position = Point(CENTER_X, CENTER_Y)
+        label = Label(text, position)
+        cast.add_actor(DIALOG_GROUP, label)
+
+
+
+    def _add_score(self, cast):
+        # cast.clear_actors(SCORE_GROUP)
+        # cast.add_actor(SCORE_GROUP, label)
+        return
+
+    def _add_stats(self, cast):
+        # cast.clear_actors(STATS_GROUP)
+        # stats = Stats()
+        # cast.add_actor(STATS_GROUP, stats)
+        return
+
+    def _add_rackets(self, cast):
+        # cast.clear_actors(RACKET_GROUP)
+        # cast.add_actor(RACKET_GROUP, racket)
+        return
+
+    # ----------------------------------------------------------------------------------------------
+    # scripting methods
+    # ----------------------------------------------------------------------------------------------
+    def _add_initialize_script(self, script):
+        script.clear_actions(INITIALIZE)
+        script.add_action(INITIALIZE, self.INITIALIZE_DEVICES_ACTION)
+
+    def _add_load_script(self, script):
+        script.clear_actions(LOAD)
+        script.add_action(LOAD, self.LOAD_ASSETS_ACTION)
+    
+    def _add_output_script(self, script):
+        script.clear_actions(OUTPUT)
+        script.add_action(OUTPUT, self.START_DRAWING_ACTION)
+        script.add_action(OUTPUT, self.DRAW_HUD_ACTION)
+        script.add_action(OUTPUT, self.DRAW_BALL_ACTION)
+        script.add_action(OUTPUT, self.DRAW_BRICKS_ACTION)
+        script.add_action(OUTPUT, self.DRAW_RACKET_ACTION)
+        script.add_action(OUTPUT, self.DRAW_DIALOG_ACTION)
+        script.add_action(OUTPUT, self.END_DRAWING_ACTION)
+
+    def _add_release_script(self, script):
+        script.clear_actions(RELEASE)
+        script.add_action(RELEASE, self.RELEASE_DEVICES_ACTION)
+    
+    def _add_unload_script(self, script):
+        script.clear_actions(UNLOAD)
+        script.add_action(UNLOAD, self.UNLOAD_ASSETS_ACTION)
+        
+    def _add_update_script(self, script):
+        script.clear_actions(UPDATE)
+        script.add_action(UPDATE, self.MOVE_BALL_ACTION)
+        script.add_action(UPDATE, self.MOVE_RACKET_ACTION)
+        script.add_action(UPDATE, self.COLLIDE_BORDERS_ACTION)
+        script.add_action(UPDATE, self.COLLIDE_BRICKS_ACTION)
+        script.add_action(UPDATE, self.COLLIDE_RACKET_ACTION)
+        script.add_action(UPDATE, self.MOVE_RACKET_ACTION)
+        script.add_action(UPDATE, self.CHECK_OVER_ACTION)
